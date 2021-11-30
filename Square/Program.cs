04870d7b953384cc0a19d84ea244b51f4f80c508
Anastasiia <Anastasiia@DESKTOP-9GJG6D4>
2021-11-30 10:57:36 +0300

    Добавьте файлы проекта.

---
+++ Square/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S, V;
+            Sq(a, out S, out V);
+            Console.WriteLine(S);
+            Console.WriteLine(V);
+            Console.ReadKey();
+        }
+        static void Sq(int a, out double S, out double V)
+        {
+            S = 6*a*a;
+            V = a*a*a; 
+        }
+    }
+}
---
+++ Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            double S;
+            TrP(a, b, c, out S);
+            Console.WriteLine(S);
+            Console.ReadKey();
+        }
+        static void TrP(int a, int b, int c, out double S)
+        {
+            double p = (a + b + c) / 2;
+            S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
+
